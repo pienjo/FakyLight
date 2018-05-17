1cf05f79456f65b4b0c856b76ed6442107ea6cfc
Martijn van Buul <martijn.van.buul@gmail.com>
2018-05-17 23:03:13 +0200

    getHueValueHistogram test, fails

--- nanojpeg/test/colorRoutines_test.cpp
+++ nanojpeg/test/colorRoutines_test.cpp
@@ -183,3 +183,105 @@ TEST(HSVtoRGB, FullMagenta)
   ASSERT_EQ(0, g);
   ASSERT_EQ(254, b);
 }
+
+TEST(getHueValueHistogram, ShadesofGray)
+{
+  // arrange
+  const uint8_t testImageContents[] = {
+    0xff, 0xff, 0xff, // white
+    0x7f, 0x7f, 0x7f, // grey
+    0x0 , 0x0,  0x0 , // black 
+    };
+  struct image testImage;
+  testImage.data = (uint8_t *) testImageContents;
+  testImage.width = 3;
+  testImage.height = 1;
+  testImage.length = sizeof(testImageContents);
+
+  uint32_t hueHistogram[256] = { 0 };
+  uint32_t valueHistogram[256] = { 0 };
+  uint8_t avgSat;
+
+  // Act
+  uint32_t total = getHueValueHistogram(&testImage, hueHistogram, valueHistogram, &avgSat, 0, 0, 3,1);
+
+  // Assert
+  ASSERT_EQ(0, avgSat);
+  ASSERT_EQ(1, valueHistogram[255]);
+  ASSERT_EQ(1, valueHistogram[0x7f]);
+  ASSERT_EQ(1, valueHistogram[0]);
+}
+
+TEST(getHueValueHistogram, ShadesOfRed)
+{
+  // arrange
+  const uint8_t testImageContents[] = {
+    0xff, 0x00, 0x00, // Red
+    0x7f, 0x00, 0x00, // dark red
+    0xff , 0x7f,  0x7f , // pink 
+    };
+
+  struct image testImage;
+  testImage.data = (uint8_t *) testImageContents;
+  testImage.width = 3;
+  testImage.height = 1;
+  testImage.length = sizeof(testImageContents);
+
+  uint32_t hueHistogram[256] = { 0 };
+  uint32_t valueHistogram[256] = { 0 };
+  uint8_t avgSat;
+
+  // Act
+  uint32_t total = getHueValueHistogram(&testImage, hueHistogram, valueHistogram, &avgSat, 0, 0, 3,1);
+
+  // Assert
+  ASSERT_EQ(211, avgSat);
+  ASSERT_EQ(2, valueHistogram[255]);
+  ASSERT_EQ(1, valueHistogram[0x7f]);
+  
+  for (int h = 1; h < 256; ++h)
+  {
+    if (hueHistogram[h] > 0)
+    {
+      ASSERT_EQ(total, hueHistogram[h]);
+      ASSERT_EQ( 0, h);
+    }
+  }
+}
+
+TEST(getHueValueHistogram, ShadesOfGreen)
+{
+  // arrange
+  const uint8_t testImageContents[] = {
+    0x00, 0xff, 0x00,  // Green
+    0x00, 0x7f, 0x00,  // dark green
+    0x7f, 0xff , 0x7f, // pastel green
+    };
+
+  struct image testImage;
+  testImage.data = (uint8_t *) testImageContents;
+  testImage.width = 3;
+  testImage.height = 1;
+  testImage.length = sizeof(testImageContents);
+
+  uint32_t hueHistogram[256] = { 0 };
+  uint32_t valueHistogram[256] = { 0 };
+  uint8_t avgSat;
+
+  // Act
+  uint32_t total = getHueValueHistogram(&testImage, hueHistogram, valueHistogram, &avgSat, 0, 0, 3,1);
+
+  // Assert
+  ASSERT_EQ(211, avgSat);
+  ASSERT_EQ(2, valueHistogram[255]);
+  ASSERT_EQ(1, valueHistogram[0x7f]);
+  
+  for (int h = 1; h < 256; ++h)
+  {
+    if (hueHistogram[h] > 0)
+    {
+      ASSERT_EQ(total, hueHistogram[h]);
+      ASSERT_EQ( 84, h);
+    }
+  }
+}
