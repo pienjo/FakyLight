1361aec5a37522b408764254f09384a6d70de034
Martijn van Buul <martijn.van.buul@gmail.com>
2018-06-21 11:52:31 +0200

    Add timeout exception

--- Faky++/ImageSource.h
+++ Faky++/ImageSource.h
@@ -2,6 +2,7 @@
 #define IMAGESOURCE_H
 
 #include "Image.h"
+#include <stdexcept>
 
 class ImageSource
 {
@@ -9,6 +10,14 @@ class ImageSource
     virtual ~ImageSource() { }
 
     virtual Image FetchImage() = 0;    
+
+    class timeout_error : public std::runtime_error
+    {
+      public:
+	timeout_error() : std::runtime_error("Timeout")
+	{
+	}
+    };
 };
 
 #endif
