e868154039e6041f30e8eacec53e2e0474e5965b
Martijn van Buul <martijn.van.buul@gmail.com>
2018-06-23 21:25:13 +0200

    Fetch: Accept filename as argument

--- Faky++/fetch-main.cpp
+++ Faky++/fetch-main.cpp
@@ -1,12 +1,17 @@
 #include "RoapSource.h"
 #include "ImageRoutines.h"
 
-int main(void)
+int main(int argc, const char *argv[])
 {
   RoapSource source("192.168.64.233", 855905);
-	
+
+  const char *filename = "fetch.ppm";  
+
+  if (argc > 1)
+    filename = argv[1];
+
   source.FetchImage();
   Image img = source.FetchImage();
-  ImageRoutines::WritePPM(img, "fetch.ppm");
+  ImageRoutines::WritePPM(img, filename);
   return 0;
 }
