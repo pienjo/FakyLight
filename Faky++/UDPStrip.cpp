701b2686aa070ffad19c55c7dfc0bd723edfe14e
Martijn van Buul <martijn.van.buul@gmail.com>
2018-07-22 08:51:00 +0200

    Include some debugging info on address failures

--- Faky++/UDPStrip.cpp
+++ Faky++/UDPStrip.cpp
@@ -1,12 +1,19 @@
 #include "UDPStrip.h"
+#include <stdexcept>
 
 UDPStrip::UDPStrip(const char *hostname, uint16_t port)
 {
   mSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
+  if (0 == mSocket)
+  {
+    perror("socket");
+  }
   mSockAddr.sin_family = AF_INET;
   mSockAddr.sin_port = htons(port);
-  inet_aton(hostname, &mSockAddr.sin_addr);
+  if (0 == inet_aton(hostname, &mSockAddr.sin_addr))
+  {
+    throw std::runtime_error("Error converting UDP address");
+  }
 }
 
 void UDPStrip::SetContents(const std::vector<RGBColor> &contents)
